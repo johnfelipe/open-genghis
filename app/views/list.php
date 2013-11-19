d7461f0cf9d7e14fced0990b7a08325a021e78c2
kadaki01 <juanljr2@gmail.com>
2013-11-19 12:06:02 -0800

    Added basic layout

---
+++ app/views/list.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <title>Genghis</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="A Khan Academy authoring tool" />
+        <meta name="keywords" content="khan, academy, genghis, open source, uc3m, authoring tool, authoring, tool" />
+        <meta name="author" content="Juan Luis" />
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <script type="text/javascript" src="js/modernizr.custom.29473.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <!-- Codrops top bar -->
+            <header>
+            <h1>Genghis <span>another Khan</span></h1>
+            <h2>Works better, harder, faster, stronger.</h2>
+            </p>
+            </header>
+            <section class="ac-container">
+            <div>
+                <input id="ac-1" name="accordion-1" type="checkbox" />
+                <label for="ac-1">MATH</label>
+                <article class="ac-small">
+                <p>All the items for the math course are displayed here, add delete or modify exercises using the options below.</p>
+                </article>
+            </div>
+            </section>
+        </div>
+    </body>
+</html>
